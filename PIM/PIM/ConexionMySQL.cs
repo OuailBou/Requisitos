0570d385cc9a09b3d77a3fee72eaab44faf7b2fd
OuailBou <156536081+OuailBou@users.noreply.github.com>
2024-11-21 19:32:57 +0100

    Update ConexionMySQL.cs

--- PIM/PIM/ConexionMySQL.cs
+++ PIM/PIM/ConexionMySQL.cs
@@ -1,60 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using MySql.Data.MySqlClient;
-
 
 namespace PIM
 {
-    class ConexionMySQL : Connection
+    class ConexionMySQL : Connection, IDisposable
     {
         private MySqlConnection connection;
-
         private string cadenaConexion;
 
         public ConexionMySQL()
         {
-
             cadenaConexion = "Database=" + database +
-
-                "; DataSource=" + server +
-
-                "; User Id=" + user +
-
-                "; Password=" + password;
+                             "; DataSource=" + server +
+                             "; User Id=" + user +
+                             "; Password=" + password;
 
             connection = new MySqlConnection(cadenaConexion);
-
         }
 
         public MySqlConnection GetConnection()
-
-        // Conecta con la base de datos
         {
+            // Cerrar la conexión si ya está abierta
+            CloseConnection();
 
+            // Conectar con la base de datos
             try
             {
-
                 if (connection.State != System.Data.ConnectionState.Open)
                 {
-
                     connection.Open();
-
                 }
-
             }
-
             catch (Exception e)
             {
-
                 Console.WriteLine(e.ToString());
-
             }
 
             return connection;
+        }
 
+        public void CloseConnection()
+        {
+            // Cerrar la conexión si está abierta
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            // Asegurarse de que la conexión se cierra y se libera correctamente
+            CloseConnection();
+            if (connection != null)
+            {
+                connection.Dispose();
+                connection = null;
+            }
         }
     }
 }
